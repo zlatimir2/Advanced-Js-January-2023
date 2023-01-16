28e337e670da5af6d043ed8089765c08165d10d7
zlatimir2 <zlatimir2@gmail.com>
2023-01-16 22:20:37 +0200

    new commit

--- Syntax, Functions and Statements - Exercise/1. Fruit.js
+++ Syntax, Functions and Statements - Exercise/1. Fruit.js
@@ -1,3 +1,5 @@
-function name(params) {
-  console.log('kur');
+function solve(fruit, grams, price) {
+  let fruitInKg = grams / 1000;
+  console.log(`I need $${(price * fruitInKg).toFixed(2)} to buy ${fruitInKg.toFixed(2)} kilograms ${fruit}.`);
 }
+solve('orange', 2500, 1.80)
