e6193f380de1e118ecb172625c6e92a8e92f5d0e
zlatimir2 <zlatimir2@gmail.com>
2023-01-19 23:15:30 +0200

    new exercies

---
+++ Arrays and Nested Arrays - Lab/01. Even Position Elements.js
@@ -0,0 +1,11 @@
+function solve(arr) {
+  let result = ''
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 == 0) {
+      result += `${arr[i]} `;
+    }
+  }
+  console.log(result);
+}
+solve(['20', '30', '40',
+  '50', '60']);
