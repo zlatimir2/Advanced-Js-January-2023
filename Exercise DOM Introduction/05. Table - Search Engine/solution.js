1d62213f4a3fadf0e1044f70189d4b3d5c313ee4
zlatimir2 <zlatimir2@gmail.com>
2023-01-25 11:42:42 +0200

    .

--- Exercise DOM Introduction/05. Table - Search Engine/solution.js
+++ Exercise DOM Introduction/05. Table - Search Engine/solution.js
@@ -1,8 +1,23 @@
 function solve() {
+   const input = document.querySelector('#searchField');
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      Array.from(document.querySelectorAll('tbody tr')).forEach((row) => {
+         if (row.textContent.toLowerCase()
+            .includes(input.value
+               .toLowerCase()
+               .trim())) {
+            row.classList.add('select')
+
+         } else {
+            row.classList.remove('select')
+         }
+
+      })
+
+      input.value = '';
 
    }
+
 }
